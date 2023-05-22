381f7de4d29660808bd7ae9f2347881b7a96884c
mohan-246 <mohan5497306@gmail.com>
2023-05-22 22:27:32 +0530

    updated database connectivity

--- app.js
+++ app.js
@@ -1,31 +1,70 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const app = express();
+const dotenv=require("dotenv");
 
+dotenv.config();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-var tasks=["Buy Food", "Cook Food", "Eat Food"];
+
 app.use(express.static("public"));
-
-
-app.get("/", function(req, res){ //ss
-
-var today=new Date();
-var options={
-    weekday:"long",
-    day:"numeric",
-    month:"long"
+var today="To do";
+    const uri=`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.t4yeixy.mongodb.net/?retryWrites=true&w=majority`;
+ mongoose.connect(uri);
+const TaskSchema={
+    task: String
 };
+const Task=mongoose.model("Task",TaskSchema);
+const task1=new Task({
+    task:"welcome to your to do list!"
+});
+const task2=new Task({
+    task:"Hit the + button to add a new task"
+});
+const task3=new Task({
+    task:"<-- hit this to delete an item"
+});
+const DefItems=[task1,task2,task3];
 
-var date=today.toLocaleDateString('en-US',options);
-res.render('list',{dayy:date,taskk:tasks});
+
+
+app.get("/", function(req, res){ 
+    Task.find({})
+    .then(function(tasks){
+        if (tasks.length===0){
+            Task.insertMany(DefItems)
+       .then(function(){
+          
+          }).catch(function(err){
+             console.log(err);
+         });
+         res.redirect("/");
+        }
+        else{
+       
+        res.render('list',{dayy:today,taskk:tasks});
+        }
+    });
+
 });
 app.post("/", function(req, res){
-    var task=req.body.newtask;
-    tasks.push(task);
+    var newitem=req.body.newtask;
+    const newtaskk=new Task({
+        task:newitem
+    });
+    newtaskk.save();
     res.redirect("/");
 })
+app.post("/delete", function(req, res){
+const remove=req.body.checkbox;
+Task.findByIdAndDelete(remove)
+.then(function(){
+    console.log("Task deleted");
+    res.redirect("/");
+});
+
+});
 app.listen(3000,function(){
     console.log("Listening");
 });
